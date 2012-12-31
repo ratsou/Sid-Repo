c399b933d317d75a0e3a69d860904f0cf889973b
sidnan <sid.nandhan@gmail.com>
2012-12-31 15:51:14 -0500

    First Commit

---
+++ HelloMahout/src/com/test/mahout/TestMht.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.test.mahout;
+
+/**
+ * @author Siddharth
+ *
+ */
+public class TestMht {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
